7ce1a945ecc7426c26fe994bc4b5f277c5928dac
gourigupta <49197168+gauriguptaa@users.noreply.github.com>
2021-08-04 23:57:12 +0530

    day-13 Debugging , Day-14 HigherLowerGameProject

---
+++ day-13-Python/day-13-13-exercise-2.py
@@ -0,0 +1,8 @@
+#Odd or Even Debug
+number = int(input("Which number do you want to check?"))
+
+#if number % 2 = 0: This was fixed
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
