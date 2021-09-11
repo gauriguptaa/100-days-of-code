94fbd8a82a8c34dbbec09a49291eb6a30c70d289
gauriguptaa <49197168+gauriguptaa@users.noreply.github.com>
2021-09-11 19:40:26 +0530

    Update main.py

--- data_science_projects/mail-merge-project/main.py
+++ data_science_projects/mail-merge-project/main.py
@@ -4,7 +4,6 @@ if __name__ == '__main__':
         name = name.strip("\n")
         send_mail = open(f"Ready_to_send\letter_for_{name}.txt", mode="w")
         with open("starting_letter.txt") as file:
-            content = file.readlines()
-            content[0] = content[0].replace("name", name)
-            content_string = ''.join(content)
-            send_mail.write(content_string)
+            content = file.read()
+            content = content.replace("name", name)
+            send_mail.write(content)
